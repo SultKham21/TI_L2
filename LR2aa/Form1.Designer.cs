e1cf204fd94dd457bb1cbf3871b9f2930024df24
SultKham21 <khamroevsulton2001@gmail.com>
2021-04-14 21:04:29 +0300

    laborat_2

---
+++ LR2aa/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace protect_inf_LR1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.shiphr = new System.Windows.Forms.Button();
+            this.buttonDecipher = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbn_p = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbn_q = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox_d = new System.Windows.Forms.TextBox();
+            this.textBox_n = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // shiphr
+            // 
+            this.shiphr.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.shiphr.Location = new System.Drawing.Point(13, 115);
+            this.shiphr.Margin = new System.Windows.Forms.Padding(4);
+            this.shiphr.Name = "shiphr";
+            this.shiphr.Size = new System.Drawing.Size(200, 41);
+            this.shiphr.TabIndex = 1;
+            this.shiphr.Text = "Encrypt";
+            this.shiphr.UseVisualStyleBackColor = true;
+            this.shiphr.Click += new System.EventHandler(this.buttonShifr_Click);
+            // 
+            // buttonDecipher
+            // 
+            this.buttonDecipher.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.buttonDecipher.Location = new System.Drawing.Point(244, 115);
+            this.buttonDecipher.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonDecipher.Name = "buttonDecipher";
+            this.buttonDecipher.Size = new System.Drawing.Size(200, 41);
+            this.buttonDecipher.TabIndex = 2;
+            this.buttonDecipher.Text = "Decrypt";
+            this.buttonDecipher.UseVisualStyleBackColor = true;
+            this.buttonDecipher.Click += new System.EventHandler(this.buttonDeshiphr);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(11, 43);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "p =";
+            // 
+            // textBox_p
+            // 
+            this.tbn_p.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.tbn_p.Location = new System.Drawing.Point(61, 40);
+            this.tbn_p.Margin = new System.Windows.Forms.Padding(4);
+            this.tbn_p.Name = "textBox_p";
+            this.tbn_p.Size = new System.Drawing.Size(152, 30);
+            this.tbn_p.TabIndex = 4;
+            this.tbn_p.Text = "101";
+            this.tbn_p.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(12, 80);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "q =";
+            // 
+            // textBox_q
+            // 
+            this.tbn_q.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.tbn_q.Location = new System.Drawing.Point(61, 77);
+            this.tbn_q.Margin = new System.Windows.Forms.Padding(4);
+            this.tbn_q.Name = "textBox_q";
+            this.tbn_q.Size = new System.Drawing.Size(152, 30);
+            this.tbn_q.TabIndex = 6;
+            this.tbn_q.Text = "103";
+            this.tbn_q.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(8, 3);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Numbers:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.Location = new System.Drawing.Point(239, 43);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "d =";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label5.Location = new System.Drawing.Point(240, 81);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 25);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "n =";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label6.Location = new System.Drawing.Point(239, 3);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(111, 25);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Secret key:";
+            // 
+            // textBox_d
+            // 
+            this.textBox_d.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox_d.Location = new System.Drawing.Point(289, 39);
+            this.textBox_d.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_d.Name = "textBox_d";
+            this.textBox_d.Size = new System.Drawing.Size(151, 30);
+            this.textBox_d.TabIndex = 11;
+            this.textBox_d.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBox_n
+            // 
+            this.textBox_n.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox_n.Location = new System.Drawing.Point(289, 77);
+            this.textBox_n.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_n.Name = "textBox_n";
+            this.textBox_n.Size = new System.Drawing.Size(151, 30);
+            this.textBox_n.TabIndex = 12;
+            this.textBox_n.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(453, 169);
+            this.Controls.Add(this.textBox_n);
+            this.Controls.Add(this.textBox_d);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbn_q);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbn_p);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonDecipher);
+            this.Controls.Add(this.shiphr);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "RSA(EC/DC)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button shiphr;
+        private System.Windows.Forms.Button buttonDecipher;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbn_p;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbn_q;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox_d;
+        private System.Windows.Forms.TextBox textBox_n;
+    }
+}
+
---
+++ LR2aa/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Numerics;
+using System.Windows.Forms;
+
+namespace protect_inf_LR1
+{
+    public partial class Form1 : Form
+    {
+        char[] characters = new char[] { 'A', 'B', 'C', 'D', 'E', 
+                                         'F', 'G', 'H', 'I', 'J',
+                                         'K', 'L', 'M', 'N', 'O', 
+                                         'P', 'Q', 'R', 'S', 'T', 
+                                         'U', 'V', 'W', 'X', 'Y', 
+                                         'Z', ' '};
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonShifr_Click(object sender, EventArgs evnt)
+        {
+            if ((tbn_p.Text.Length > 0) && (tbn_q.Text.Length > 0))
+            {
+                long p = Convert.ToInt64(tbn_p.Text);
+                long q = Convert.ToInt64(tbn_q.Text);
+
+                if (CheckSimple(p) && CheckSimple(q))
+                {
+                    string strin = "";
+                    StreamReader stre = new StreamReader("ent.txt");
+
+                    while (!stre.EndOfStream)
+                    {
+                        strin += stre.ReadLine();
+                    }
+
+                    stre.Close();
+                    strin = strin.ToUpper();
+                    long n = p * q;
+                    long fi = (p - 1) * (q - 1);
+                    long d = CalcD(fi);
+                    long e = CalcE(d, fi);
+                    List<string> result = RSAE(strin, e, n);
+                    StreamWriter stwr = new StreamWriter("on.txt");
+
+                    foreach (string item in result)
+                        stwr.WriteLine(item);
+                    stwr.Close();
+
+                    textBox_d.Text = d.ToString();
+                    textBox_n.Text = n.ToString();
+
+                    Process.Start("on.txt");
+                }
+                else
+                    MessageBox.Show("p and q - must be simple");
+            }
+            else
+                MessageBox.Show("no p and q");
+        }
+
+        private void buttonDeshiphr(object sender, EventArgs e)
+        {
+            if ((textBox_d.Text.Length > 0) && (textBox_n.Text.Length > 0))
+            {
+                long d = Convert.ToInt64(textBox_d.Text);
+                long n = Convert.ToInt64(textBox_n.Text);
+
+                List<string> input = new List<string>();
+
+                StreamReader sr = new StreamReader("on.txt");
+
+                while (!sr.EndOfStream)
+                {
+                    input.Add(sr.ReadLine());
+                }
+
+                sr.Close();
+
+                string result = RSAD(input, d, n);
+
+                StreamWriter sw = new StreamWriter("ow.txt");
+                sw.WriteLine(result);
+                sw.Close();
+
+                Process.Start("ow.txt");
+            }
+            else
+                MessageBox.Show("you must enter secret key");
+        }
+
+        private List<string> RSAE(string s, long eg, long n)
+        {
+            List<string> result = new List<string>();
+            BigInteger big;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                int index = Array.IndexOf(characters, s[i]);
+                big = new BigInteger(index);
+                big = BigInteger.Pow(big, (int)eg);
+                BigInteger nt = new BigInteger((int)n);
+                big = big % nt;
+                result.Add(big.ToString());
+            }
+
+            return result;
+        }
+
+        private long CalcD(long al)
+        {
+            long d = al - 1;
+
+            for (long i = 2; i <= al; i++)
+                if ((al % i == 0) && (d % i == 0))
+                {
+                    d--;
+                    i = 1;
+                }
+
+            return d;
+        }
+
+        private string RSAD(List<string> input, long d, long n)
+        {
+            string result = "";
+            BigInteger big;
+
+            foreach (string item in input)
+            {
+                big = new BigInteger(Convert.ToDouble(item));
+                big = BigInteger.Pow(big, (int)d);
+                BigInteger nt = new BigInteger((int)n);
+                big = big % nt;
+                int index = Convert.ToInt32(big.ToString());
+                result += characters[index].ToString();
+            }
+
+            return result;
+        }
+
+        private long CalcE(long d, long al)
+        {
+            long e = 10;
+
+            while (true)
+            {
+                if ((e * d) % al == 1)
+                    break;
+                else
+                    e++;
+            }
+
+            return e;
+        }
+
+        private bool CheckSimple(long n)
+        {
+            if (n < 2)
+                return false;
+
+            if (n == 2)
+                return true;
+
+            for (long i = 2; i < n; i++)
+                if (n % i == 0)
+                    return false;
+
+            return true;
+        }
+    }
+}
